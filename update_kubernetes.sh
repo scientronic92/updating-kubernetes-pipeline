8127b08c1c7001c5915d1a365caf649adaf1285b
omer aiman <ew18@protonmail.com>
2020-02-23 20:46:54 +0300

    changing the image

--- update_kubernetes.sh
+++ update_kubernetes.sh
@@ -9,9 +9,10 @@ dockerpath=scientronic92/flaskapp:5
 # Step 2
 # updating the running container with kubernetes
 # kubectl rolling-update flaskapp flaskapp-1 --image=scientronic92/flaskapp:5
-# kubectl set image deployment flaskapp flaskapp=scientronic92/flaskapp:5 --record
 
-kubectl apply -f deployapp.yml
+kubectl set image deployment flaskapp flaskapp=nginx --record
+
+# kubectl apply -f deployapp.yml
 
 
 # Step 3:
